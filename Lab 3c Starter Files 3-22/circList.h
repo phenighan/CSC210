28cc7cf6c8741ee7e2dd3d9806544bc70ced9899
Paris Henighan <phenighan34@gmail.com>
2017-03-22 22:16:42 -0400

    Add files via upload

---
+++ Lab 3c Starter Files 3-22/circList.h
@@ -0,0 +1,52 @@
+/* CircList.h Specifications and ClassDefinitions for Lab 3
+   Header file for the Circular Linked List lab assignment
+   Implements the “Josephus problem” as an ADT
+   These member names must be used in your class without modification.
+   The two public functions for reading and printing will be commented out in your
+   final version.
+   They may be used for starting the class until overloaded operators are implemented
+   Programmer Jeff Goldstein  02/14/2017
+*/
+
+#ifndef CircList_H
+#define CircList_H
+
+using namespace std;
+
+struct node
+{
+    string myName;                            // holds name of the prisoner
+    node*  next;                              // link points to the next node
+};
+
+class CircList
+{
+    // operator overloading for insertion & extraction streams
+    friend ostream& operator<< (ostream &, const CircList & );
+    friend istream& operator>> (istream &, CircList & );
+
+    public:
+        CircList();                           // constructor for linked list
+        ~CircList();                          // destructor for linked list
+        CircList(const CircList & source);    // copy constructor
+        const CircList& operator= (const CircList &);  // * overload assignment op *
+
+        int size() const;                     // returns number of nodes in list
+        void addNode(string);                 // adds a node with data name
+        string delete7thNode();               // deletes node after adv. pointer by 7
+        bool find(string prisoner);           // reset pointers based upon lottery
+        bool isEmpty();                       // returns true if list is empty
+        //void printList(ostream&);           // temporary not used in finished Lab 3
+        //void readData(istream&);            // temporary not used in finished Lab 3
+
+   private:
+        node *head;                           // points head of list in circ. order
+        node *last;                           // points to last node (before head)
+        int  count;                           // keeps count of # nodes in the list
+     	void copyList(const CircList & rhs);  // a private copy function
+        void deleteList();                    // deletes the entire list
+};
+
+#endif
+
+
---
+++ Lab 3c Starter Files 3-22/circMain.cpp
@@ -0,0 +1,55 @@
+// circMain.cpp (an excellent start to Lab3 Circular Linked List)
+// This does create a circularly linked list, but it's only a start for your class.
+// Linked List of names (simplified read & write.  Not a class at all!)
+// reading the names, traversing the list, & writing to the console & file
+// Written by Jeff Goldstein, TCC Adjunct Professor, Virginia Beach Campus
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdlib.h>
+#include "CircList.cpp"
+
+using namespace std;
+
+ifstream fin ("CircInput.txt");
+ofstream fout("CircOutput.txt");
+
+void title(string);                             // prototypes for functions called
+void process(CircList list);
+
+int main()
+{
+    CircList list1;
+    CircList list2;
+    fin >> list1;
+    list2 = list1;
+
+    title("LIST");
+    process(list2);
+    fout << "Original list1.  List size = " << list1.size() << endl;
+    cout << "Original list1.  List size = " << list1.size() << endl;
+    fout <<list1;
+    fout << "Original list2.  List size = " << list2.size() << endl;
+    cout << "Original list2.  List size = " << list2.size() << endl;
+    fout <<list2;
+    fin.close();
+	fout.close();                               // close the output file.
+	return 0;
+}
+
+void process(CircList list)
+{
+    cout << "List size = " << list.size() << endl;
+    fout << list;
+    cout << list.find("Gore") << endl;
+    cout << list;
+}
+
+void title(string st)
+{
+    cout << "HERE'S THE CIRCULARLY LINKED " << st << endl;
+    cout << "====== === ========== ====== ====" << endl << endl;
+    fout << "HERE'S THE CIRCULARLY LINKED " << st << endl;
+    fout << "====== === ========== ====== ====" << endl << endl;
+}
