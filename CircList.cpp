04dc19974d00082c9fc5e3eb385563cedd973734
Paris Henighan <phenighan34@gmail.com>
2017-03-28 06:57:48 -0400

    linked list 3.0

---
+++ CircList.cpp
@@ -0,0 +1,190 @@
+// CircList.cpp (impmentation file for CircList)
+// Written by:  Paris Henighan
+
+#include "CircList.h"
+extern ifstream fin;
+extern ofstream fout;
+
+CircList::CircList()
+{
+    head = NULL;
+    last = NULL;
+    count = 0;          // initialize length of  to zero
+}
+
+CircList::~CircList()
+{
+    deleteList();
+}
+
+CircList::CircList(const CircList & source)
+{
+    head = last;
+    copyList(source);
+}
+
+void CircList::copyList(const CircList & rhs)
+{
+    node *newNode;
+    node *current;
+
+    if (head != last)       //if list is not empty
+        deleteList();       //delete target list
+    if (rhs.head == NULL)
+    {
+        head = NULL;
+        last = NULL;
+        count = 0;
+    }
+    else
+    {
+        current = rhs.head;     //current points to target
+        count = rhs.count;       //copy source's count
+
+        //copy the first node
+        head = new node;
+        head->myName = current->myName; //copies info
+        head->next = head;      //points to itself
+        last = head;            //assign last to head
+        current = current->next;        //traverse to next node
+
+        //copy remaining list
+        while (current != rhs.head)
+        {
+            newNode = new node;     //create node
+            newNode->myName = current->myName;  //copy name
+            newNode->next = head;       //set link to head
+            last->next = newNode;       //attach newNode after last
+            last = newNode;         //make last point to actual last node
+            current = current->next; //advance current pointer
+        }   //end while
+    }   //end else
+}
+const CircList& CircList::operator=(const CircList & rhs) //rhs = right hand side
+{
+    if (this != &rhs)   //avoid self assignment
+    {
+        copyList(rhs);  //call the copyList function
+    }
+    return *this;
+}
+int CircList::size() const
+{
+    return count;
+}
+
+void CircList::deleteList()
+{
+    node * temp;
+    while (head != last)
+    {
+        temp = head;
+        head = head->next;
+        cout << "Prisoner destroyed" << endl;
+        delete temp;
+    }
+    delete head;
+    cout << "Prisoner destroyed" << endl;
+    last = NULL;
+    temp = NULL;
+    count = 0;
+}
+
+void CircList::addNode(string name)
+{
+	node * loc = new node;
+    loc-> myName = name;		             // assigns the name to the private member
+
+	if(count != 0)		                     // if list already exists
+	{
+		loc->next = last->next;				 // assign the last next pointer
+		last->next = loc;		             // advance the current pointer
+		last = loc;				             // connect the node to the last
+	}
+	else                                     // it must be a new list
+    {
+		last = loc;			                 // assign the last pointer
+        head = loc;                          // assign the head pointer
+        last->next = last;                   // heat points to itself
+    }
+	count++;				                 // increase the list size by 1
+}
+
+///I'm off here somewhere.
+/*string CircList::delete7thNode()
+{
+    node *cur;
+    bool found;
+    if (head == last)
+        cout << "cannot delete from empty list."<< endl;
+    else if (cur->next)
+    {
+        cur = head;
+        head = head->next;
+        if (head != last)
+            head->next = last;
+        else
+            last = last;
+
+        count --;
+
+        delete cur;
+    }
+
+} */
+
+bool CircList::find(string name)
+{
+    node *cur;
+   bool found = false;
+    cur = head;
+    while (cur != last && !found)
+    {
+        if (cur->myName == name)
+            {
+            found = true;
+            }
+        else if (cur->myName != name)
+          {
+        head = head->next;
+        last = last->next;
+        cur = cur->next;
+          }
+    } //end search
+return found;
+}
+
+istream& operator>> (istream & inData, CircList & cObj)
+{
+    string name;
+
+    inData >> name;                                // note the priming read 1st item
+    while( !inData.eof() )
+    {
+        cObj.addNode(name);
+       // cout << name << endl;                /// for troubleshooting
+        inData >> name;
+    }
+    return inData;
+}
+
+ostream& operator<< (ostream & out, const CircList & obj)
+{
+    node *curr = obj.head;                          // make a copy of the head pointer
+    cout << fixed  << showpoint << left;
+    out << fixed  << showpoint << left;
+
+	// write the list
+    cout << setw(8) << curr->myName;            // priming write name on the console
+    out << setw(8) << curr->myName;            // priming output name to the file
+	while (curr != obj.last)			            // while not at the last prisoner
+	{
+        curr = curr->next;			            // now, point to the next node
+        cout << setw(8) << curr->myName;        // display name on the console
+        out << setw(8) << curr->myName;        // output name to the file
+	}
+	cout << endl << endl;
+	out << endl << endl;
+    return out;
+}
+
---
+++ circMain.cpp
@@ -0,0 +1,70 @@
+// circMain.cpp (an excellent start to Lab3 Circular Linked List)
+// This does create a circularly linked list, but it's only a start for your class.
+// Linked List of names (simplified read & write.  Not a class at all!)
+// reading the names, traversing the list, & writing to the console & file
+// Written by Paris Henighan, TCC Student, Virginia Beach Campus
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdlib.h>
+#include <ctime>
+#include <string>
+#include "CircList.cpp"
+
+using namespace std;
+
+ifstream fin ("CircInput.txt");
+ofstream fout("CircOutput.txt");
+
+void title(string);                             // prototypes for functions called
+void process(CircList list);
+
+int main()
+{
+    CircList list1;
+    CircList list2;
+    fin >> list1;
+    list2 = list1;
+
+    title("LIST");
+
+    fout << "(Original list1.  List size = " << list1.size() << ')' << endl;
+    cout << "(Original list1.  List size = " << list1.size() << ')' << endl;
+    fout <<list1;
+    fout << "(Original list2.  List size = " << list2.size() << ')' << endl;
+    cout << "(Original list2.  List size = " << list2.size() << ')' << endl;
+    fout <<list2;
+    process(list2);
+    fin.close();
+	fout.close();                               // close the output file.
+	return 0;
+}
+
+void process(CircList list)
+{
+    cout << "(Copy of original.  List size = " << list.size() << ')' << endl;
+    fout << "(Copy of original.  List size = " << list.size() << ')' << endl;
+    fout << list;
+    list.find("Gore");
+    fout << list;
+    cout << list;
+    ///It just isn't working. I've tried several things. This is the closest I have come.
+    //list.delete7thNode();
+    //cout << "delete 7th node: ";
+    //fout << list;
+}
+
+void title(string st)
+{
+    fout << "   A  C I R C U L A R L Y  L I N K E D  L I S T  ( T H E  J O S E P H U S  P R O B L E M )" << endl << endl;
+    char date[10];
+    _strdate(date);
+    fout << setw(43) << "Current Date: " << date << endl << endl;
+    fout << setw(41) << " by "<< endl << endl;
+    fout << setw(47) << "Paris Henighan"<< endl << endl;
+    cout << "   A  C I R C U L A R L Y  L I N K E D  L I S T  ( T H E  J O S E P H U S  P R O B L E M )" << endl << endl;
+    cout << setw(43) << "Current Date: " << date << endl << endl;
+    cout << setw(41) << " by "<< endl << endl;
+    cout << setw(47) << "Paris Henighan"<< endl << endl;
+}
